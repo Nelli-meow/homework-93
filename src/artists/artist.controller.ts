19f8435af6e6f07c2ff5b05086ef5811c1d131a0
nelli gorlova <nelli7035@gmail.com>
2025-02-12 21:12:39 +0600

    Wrote a method for creating an artist

--- src/artists/artist.controller.ts
+++ src/artists/artist.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Artist, ArtistDocument } from '../schemas/artist.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateArtistDto } from './create-artist.dto';
 
 @Controller('artists')
 export class ArtistController {
@@ -21,8 +32,20 @@ export class ArtistController {
   }
 
   @Post()
-  createOneArtist() {
-    return { message: 'Artist was created' };
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/artists' }),
+  )
+  async createOneArtist(
+    @Body() artistData: CreateArtistDto,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    const artist = new this.artistModel({
+      name: artistData.name,
+      image: file ? `/uploads/artists/${file.filename}` : null,
+      description: artistData.description,
+    });
+
+    return artist.save();
   }
 
   @Delete(':id')
